e87eaea172a25dc5cf162a29136dd32193f169db
Aaron Sanchez <saarong12@gmail.com>
2024-01-29 19:42:01 -0600

    Added team count to localstorage to have teams persist through reload

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // Global variables
 const maxTeams = 5
-let currentTeamCount = 0
+
+let currentTeamCount = localStorage.getItem('teamCount')
 
 let teamColors = ['Blue', 'Black', 'Green', 'Orange', 'Yellow']
 
 // App structure setup.
 const root = document.querySelector('.root')
 
-const addTeamsControl = document.createElement('div')
+const addTeamsControl = document.createElement('section')
 addTeamsControl.classList.add('controls-wrapper')
 
 const teamsDisplay = document.createElement('section')
@@ -16,6 +17,23 @@ teamsDisplay.classList.add('teams-display-wrapper')
 root.appendChild(addTeamsControl)
 root.appendChild(teamsDisplay)
 
+// loop through the currentTeamCount from local storage
+// If value is greater than 0
+if (currentTeamCount > 0) {
+  for (let i = 0; i < currentTeamCount; i++) {
+    const teamCardTemplate = document.createElement('div')
+    teamCardTemplate.classList.add('team-card')
+    teamCardTemplate.classList.add(`team-${i + 1}`)
+
+    const teamNameInput = document.createElement('input')
+    teamNameInput.classList.add('team-name-input')
+    teamNameInput.placeholder = 'Team Name'
+
+    teamsDisplay.appendChild(teamCardTemplate)
+    teamCardTemplate.appendChild(teamNameInput)
+  }
+}
+
 // Button to add team cards for each team playing.
 const addTeamsBtn = document.createElement('div')
 addTeamsBtn.classList.add('btn')
@@ -23,15 +41,22 @@ addTeamsBtn.innerHTML = 'Add Teams'
 
 addTeamsControl.appendChild(addTeamsBtn)
 
-// Team card template
-
 const addTeam = () => {
   if (currentTeamCount < 5) {
     currentTeamCount++
+    localStorage.setItem('teamCount', currentTeamCount)
+
+    // Team card template
     const teamCardTemplate = document.createElement('div')
     teamCardTemplate.classList.add('team-card')
     teamCardTemplate.classList.add(`team-${currentTeamCount}`)
+
+    const teamNameInput = document.createElement('input')
+    teamNameInput.classList.add('team-name-input')
+    teamNameInput.placeholder = 'Team Name'
+
     teamsDisplay.appendChild(teamCardTemplate)
+    teamCardTemplate.appendChild(teamNameInput)
   }
   console.log(currentTeamCount)
 }
